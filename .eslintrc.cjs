dd5d64826c05685db8cf72f513ba7ca6f7e06c73
Marcos Nocetti <nocettimarcos@gmail.com>
2026-04-22 14:37:54 -0300

    chore: scaffold pnpm monorepo with TS/ESLint/Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  env: { node: true, jest: true },
+  rules: { '@typescript-eslint/no-explicit-any': 'warn' },
+};
